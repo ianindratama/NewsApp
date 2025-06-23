30e589d67b057baebaf6f49cfadab3d81c4f6a90
ianindratama <53966296+ianindratama@users.noreply.github.com>
2025-06-23 09:13:11 +0700

    implement settings feature, add dark mode

--- app/src/main/java/com/ianindratama/newsapp/presentation/settings/SettingsFragment.kt
+++ app/src/main/java/com/ianindratama/newsapp/presentation/settings/SettingsFragment.kt
@@ -1,27 +1,58 @@
 package com.ianindratama.newsapp.presentation.settings
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import com.ianindratama.newsapp.R
 import com.ianindratama.newsapp.presentation.MainActivity
+import com.ianindratama.newsapp.presentation.utils.NightMode
+import java.util.Locale
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    private lateinit var prefManager: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        prefManager = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        editor = prefManager.edit()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         (requireActivity() as MainActivity).updateAppBarTitle(getString(R.string.settings_fragment_title))
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings_preferences, rootKey)
+
+        val prefListThemes = findPreference<ListPreference>(getString(R.string.pref_key_dark))
+        prefListThemes?.setOnPreferenceChangeListener { _, newValue ->
+            val value = when (newValue) {
+                "on" -> getString(R.string.pref_dark_on)
+                "off" -> getString(R.string.pref_dark_off)
+                else -> getString(R.string.pref_dark_auto)
+            }
+
+            editor.putString(getString(R.string.pref_key_dark), value)
+            val themeMode = NightMode.valueOf(value.uppercase(Locale.US)).value
+            updateTheme(themeMode)
+
+            return@setOnPreferenceChangeListener true
+        }
+    }
+
+    private fun updateTheme(nightMode: Int): Boolean {
+        AppCompatDelegate.setDefaultNightMode(nightMode)
+        requireActivity().recreate()
+        return true
     }
 
 }
