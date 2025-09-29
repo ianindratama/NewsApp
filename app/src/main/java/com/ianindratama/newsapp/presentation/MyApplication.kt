90d1839e368a28db4a6ddbca7aedc4353cf577c0
ianindratama <53966296+ianindratama@users.noreply.github.com>
2025-09-29 13:16:51 +0700

    remove leakcanary code in MyApplication

--- app/src/main/java/com/ianindratama/newsapp/presentation/MyApplication.kt
+++ app/src/main/java/com/ianindratama/newsapp/presentation/MyApplication.kt
@@ -3,16 +3,14 @@ package com.ianindratama.newsapp.presentation
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceManager
-import com.ianindratama.newsapp.BuildConfig
 import com.ianindratama.newsapp.R
 import com.ianindratama.newsapp.core.di.databaseModule
 import com.ianindratama.newsapp.core.di.networkModule
 import com.ianindratama.newsapp.core.di.repositoryModule
-import com.ianindratama.newsapp.di.useCaseModule
 import com.ianindratama.newsapp.di.commonViewModelModule
 import com.ianindratama.newsapp.di.detailViewModelModule
+import com.ianindratama.newsapp.di.useCaseModule
 import com.ianindratama.newsapp.presentation.utils.NightMode
-import leakcanary.LeakCanary
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -36,12 +34,6 @@ class MyApplication : Application() {
                     commonViewModelModule,
                     detailViewModelModule
                 )
-            )
-        }
-
-        if (BuildConfig.DEBUG) {
-            LeakCanary.config = LeakCanary.config.copy(
-                retainedVisibleThreshold = 3
             )
         }
 
